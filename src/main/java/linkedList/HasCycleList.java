6cfffbb7e60d7b046b485dcde2f0fd93d21e1e40
B.YANG <milanyangbo@hotmail.com>
2020-03-29 17:19:44 +0800

    11

---
+++ src/main/java/linkedList/HasCycleList.java
@@ -0,0 +1,24 @@
+package linkedList;
+
+import java.util.Objects;
+
+public class HasCycleList {
+
+    public static void main(String[] args) {
+    }
+
+    public static boolean HasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (Objects.equals(slow, fast)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
