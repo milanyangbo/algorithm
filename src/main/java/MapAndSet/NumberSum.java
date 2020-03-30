218d7f613e50a1824e5e64c4f21b4b412ca98343
yangbo2 <yangbo2@wljs.com>
2020-03-30 19:12:18 +0800

    11

---
+++ src/main/java/MapAndSet/NumberSum.java
@@ -0,0 +1,14 @@
+package MapAndSet;
+
+/** https://leetcode-cn.com/problems/two-sum/ https://leetcode-cn.com/problems/3sum/ */
+public class NumberSum {
+  public static void main(String[] args) {}
+
+  public int[] twoSum(int[] nums, int target) {
+    return null;
+  }
+
+  public int[] threeSum(int[] nums, int target) {
+    return null;
+  }
+}
