9e0b81f3ed490266f346fa58cf807e872e455ca5
B.YANG <milanyangbo@hotmail.com>
2020-03-30 21:38:37 +0800

    11

--- src/main/java/MapAndSet/NumberSum.java
+++ src/main/java/MapAndSet/NumberSum.java
@@ -1,14 +1,34 @@
 package MapAndSet;
 
-/** https://leetcode-cn.com/problems/two-sum/ https://leetcode-cn.com/problems/3sum/ */
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/two-sum/ https://leetcode-cn.com/problems/3sum/
+ */
 public class NumberSum {
-  public static void main(String[] args) {}
+    public static void main(String[] args) {
+        twoSum(new int[]{3, 2, 4}, 6);
+    }
 
-  public int[] twoSum(int[] nums, int target) {
-    return null;
-  }
+    public static int[] twoSum(int[] nums, int target) {
 
-  public int[] threeSum(int[] nums, int target) {
-    return null;
-  }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int element = target - nums[i];
+            if (map.containsKey(element) && map.get(element) != i) {
+                return new int[]{i, map.get(element)};
+            }
+        }
+
+        throw new IllegalArgumentException(" no two sum");
+    }
+
+    public int[] threeSum(int[] nums, int target) {
+        return null;
+    }
 }
