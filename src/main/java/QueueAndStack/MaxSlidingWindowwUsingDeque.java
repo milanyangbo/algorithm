6c37bb924af50d328cfdb1466b3e165fa09a7f78
yangbo2 <yangbo2@wljs.com>
2020-03-30 14:30:37 +0800

    11

---
+++ src/main/java/QueueAndStack/MaxSlidingWindowwUsingDeque.java
@@ -0,0 +1,12 @@
+package QueueAndStack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MaxSlidingWindowwUsingDeque {
+
+  public int[] maxSlidingWindow(int[] nums, int k) {
+    Deque deque = new LinkedList();
+    return null;
+  }
+}
