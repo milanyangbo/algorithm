5b9a69a662e1219af6b2875b12f0fb18994e35ab
B.YANG <milanyangbo@hotmail.com>
2020-04-01 02:50:51 +0800

    11

--- src/main/java/Tree/Ancestor.java
+++ src/main/java/Tree/Ancestor.java
@@ -10,18 +10,44 @@ public class Ancestor {
 
   /** 如果有父指针，从叶子节点回溯到根节点形成链表，判断两个链表最近的交集节点 */
   public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    if (root == null || root == p || root == q) {
+    if (root == null) {
+      return null;
+    }
+
+    if (root == p || root == q) {
       return root;
     }
 
     TreeNode left = lowestCommonAncestor(root.left, p, q);
     TreeNode right = lowestCommonAncestor(root.right, p, q);
-    return left == null ? right == null ? root : root : root;
+
+    if (left == null) {
+      return right;
+    }
+    if (right == null) {
+      return left;
+    }
+    return root;
   }
 
   /** Binary Search Tree */
   public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null) {
+      return null;
+    }
 
-    return null;
+    if (root == p || root == q) {
+      return root;
+    }
+
+    if (p.val < root.val && q.val < root.val) {
+      return lowestCommonAncestor(root.left, p, q);
+    }
+
+    if (p.val > root.val && q.val > root.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    }
+
+    return root;
   }
 }
