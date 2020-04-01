35d9d2d8e4eb49fa10a605e8d9edab674f0e44c4
yangbo2 <yangbo2@wljs.com>
2020-04-01 19:05:04 +0800

    11

--- src/main/java/DFSAndBFS/Parenthesis.java
+++ src/main/java/DFSAndBFS/Parenthesis.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 /** https://leetcode-cn.com/problems/generate-parentheses/ */
 public class Parenthesis {
+  /** 局部不合法，就返回 used=n
+   *  可以将多种结果考虑成多叉树，通过深度优先搜索查找
+   * */
   public List<String> generateParenthesis(int n) {
     return null;
   }
--- src/main/java/Greedy/BuySellStocks.java
+++ src/main/java/Greedy/BuySellStocks.java
@@ -3,13 +3,12 @@ package Greedy;
 /** https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/ */
 public class BuySellStocks {
 
-  /** 贪心算法 */
+  /**
+   * 贪心算法 res = (prices[3] - prices[2]) + (prices[2] - prices[1]) + (prices[1] - prices[0])
+   * =prices[3] - prices[0] 等价于：“在索引为 0 的那一天买入，在索引为 3 的那一天卖出” “贪心算法” 在每一步总是做出在当前看来最好的选择
+   * 贪心算法时间复杂度一般是线性的，空间复杂度是常数级别的
+   */
   public int maxProfitGreedy(int[] prices) {
-    return 0;
-  }
-
-  /** 深度优先 */
-  public int maxProfitDFS(int[] prices) {
     return 0;
   }
 
