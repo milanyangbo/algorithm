d1967d2e0e658fc932c534a70e632024a0b559c6
yangbo2 <yangbo2@wljs.com>
2020-05-14 17:30:21 +0800

    11

--- src/main/java/UnionFind/FindCircleNum.java
+++ src/main/java/UnionFind/FindCircleNum.java
@@ -22,37 +22,4 @@ public class FindCircleNum {
     }
     return unionUF.count;
   }
-
-  public static class QuickUnionUF {
-    int[] roots;
-    int count;
-
-    public QuickUnionUF() {}
-
-    private int findRoot(int i) {
-      int root = i;
-      while (root != roots[root]) {
-        root = roots[root];
-      }
-      while (i != roots[root]) {
-        int temp = roots[i];
-        roots[i] = root;
-        i = temp;
-      }
-      return root;
-    }
-
-    public boolean connected(int p, int q) {
-      return findRoot(p) == findRoot(q);
-    }
-
-    public void union(int p, int q) {
-      int qroot = findRoot(q);
-      int proot = findRoot(p);
-      roots[proot] = qroot;
-      if (qroot != proot) {
-        --count;
-      }
-    }
-  }
 }
--- src/main/java/UnionFind/NumIslands.java
+++ src/main/java/UnionFind/NumIslands.java
@@ -54,39 +54,6 @@ public class NumIslands {
     return unionUF.count;
   }
 
-  public static class QuickUnionUF {
-    int[] roots;
-    int count;
-
-    public QuickUnionUF() {}
-
-    private int findRoot(int i) {
-      int root = i;
-      while (root != roots[root]) {
-        root = roots[root];
-      }
-      while (i != roots[root]) {
-        int temp = roots[i];
-        roots[i] = root;
-        i = temp;
-      }
-      return root;
-    }
-
-    public boolean connected(int p, int q) {
-      return findRoot(p) == findRoot(q);
-    }
-
-    public void union(int p, int q) {
-      int qroot = findRoot(q);
-      int proot = findRoot(p);
-      roots[proot] = qroot;
-      if (qroot != proot) {
-        --count;
-      }
-    }
-  }
-
   public int numIslandsDfs(char[][] grid) {
     if (grid.length == 0) {
       return 0;
