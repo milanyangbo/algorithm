2876ff34d90eaf0a4fe7a37eab14d354797570ee
B.YANG <milanyangbo@hotmail.com>
2020-05-24 12:10:39 +0800

    11

--- src/main/java/QueueAndStack/LongestPalindrome.java
+++ src/main/java/QueueAndStack/LongestPalindrome.java
@@ -7,39 +7,9 @@ public class LongestPalindrome {
   }
 
   public String longestPalindrome(String s) {
-    if (s.length() == 1) {
-      return s;
-    }
-    int n = s.length();
-    String p = "";
-    for (int start = 0; start < n; start++) {
-      int end = start + 1;
-      int len = 0;
-      while (end < n) {
-        if (!isVaild(s, start, end)) {
-          --end;
-          break;
-        }
-        len = end - start + 1;
-        ++end;
-      }
+    if (s == null || s.length() < 1) return "";
 
-      if (len > p.length()) {
-        p = s.substring(start, end + 1);
-      }
-    }
-    return p;
+    return s;
   }
 
-  private boolean isVaild(String s, int start, int end) {
-    int end1 = ((end + start) / 2);
-    while (start <= end1) {
-      if (s.charAt(start) != s.charAt(end)) {
-        return false;
-      }
-      end--;
-      start++;
-    }
-    return true;
-  }
 }
