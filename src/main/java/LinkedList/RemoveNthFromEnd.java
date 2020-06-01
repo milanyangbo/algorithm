5a481809d85822e583f1c748b4ed4dda3c724c1b
yangbo2 <yangbo2@wljs.com>
2020-06-01 16:26:05 +0800

    11

---
+++ src/main/java/LinkedList/RemoveNthFromEnd.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+/** https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/ */
+public class RemoveNthFromEnd {
+  public static void main(String[] args) {
+    //    ListNode head =
+    //        new ListNode(1)
+    //            .setNext(
+    //                new ListNode(2)
+    //                    .setNext(new ListNode(3).setNext(new ListNode(4).setNext(new
+    // ListNode(5)))));
+    ListNode head = new ListNode(1);
+    new RemoveNthFromEnd().removeNthFromEnd(head, 1);
+  }
+
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode prev = new ListNode(-1);
+    prev.next = head;
+    ListNode fast = prev;
+    ListNode slow = prev;
+
+    for (int i = 0; i <= n; i++) {
+      fast = fast.next;
+    }
+    while (fast != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+    return prev.next;
+  }
+}
---
+++ src/main/java/LinkedList/ReverseKGroup.java
@@ -0,0 +1,56 @@
+package LinkedList;
+
+/** https://leetcode-cn.com/problems/reverse-nodes-in-k-group/ */
+public class ReverseKGroup {
+
+  public static void main(String[] args) {
+    ListNode head =
+        new ListNode(1)
+            .setNext(
+                new ListNode(2)
+                    .setNext(new ListNode(3).setNext(new ListNode(4).setNext(new ListNode(5)))));
+
+    new ReverseKGroup().reverseKGroup(head, 3);
+  }
+
+  public ListNode reverseKGroup(ListNode head, int k) {
+    ListNode pos = new ListNode(-1);
+    pos.next = head;
+    ListNode prev = pos;
+    ListNode next;
+    ListNode cur = head;
+    while (cur != null) {
+      ListNode temp = prev;
+      int i = 0;
+      for (; i < k; i++) {
+        if (cur != null) {
+          temp.next = cur;
+          temp = cur;
+          cur = cur.next;
+        } else {
+          break;
+        }
+      }
+      if (i == k) {
+        next = temp.next;
+        temp.next = null;
+
+        ListNode node = prev.next;
+        ListNode pre1 = null;
+        ListNode cur1 = node;
+        ListNode temp1;
+        while (cur1 != null) {
+          temp1 = cur1.next;
+          cur1.next = pre1;
+          pre1 = cur1;
+          cur1 = temp1;
+        }
+        prev.next = pre1;
+        node.next = next;
+        prev = node;
+      }
+    }
+
+    return pos.next;
+  }
+}
