957bad39e4f433d99ecd057c079e7c4b2c2ee27a
yangbo2 <yangbo2@wljs.com>
2020-06-01 18:19:00 +0800

    11

---
+++ src/main/java/QueueAndStackAndArray/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package QueueAndStackAndArray;
+
+/** https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/ */
+public class RemoveDuplicates {
+
+  public static void main(String[] args) {
+    new RemoveDuplicates().removeDuplicates(new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4});
+  }
+
+  public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[i] != nums[j]) {
+        i++;
+        nums[i] = nums[j];
+      }
+    }
+    return i + 1;
+  }
+}
---
+++ src/main/java/QueueAndStackAndArray/RemoveElement.java
@@ -0,0 +1,20 @@
+package QueueAndStackAndArray;
+
+/** https://leetcode-cn.com/problems/remove-element/ */
+public class RemoveElement {
+
+  public static void main(String[] args) {
+    new RemoveElement().removeElement(new int[] {3, 2, 2, 3}, 3);
+  }
+
+  public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[j] != val) {
+        nums[i] = nums[j];
+        i++;
+      }
+    }
+    return i;
+  }
+}
