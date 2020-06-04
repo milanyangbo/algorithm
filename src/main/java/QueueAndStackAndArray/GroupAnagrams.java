ad66e68d5824cd42cff01b8a96e7cdcbc288198e
yb <milanyangbo@hotmail.com>
2020-06-04 17:19:25 +0800

    11

---
+++ src/main/java/QueueAndStackAndArray/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package QueueAndStackAndArray;
+
+import java.util.*;
+
+/** https://leetcode-cn.com/problems/group-anagrams/ */
+public class GroupAnagrams {
+
+  public static void main(String[] args) {
+    new GroupAnagrams().groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"});
+  }
+
+  public List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> result = new ArrayList<>();
+    Map<String, List<String>> map = new HashMap<>();
+    for (String str : strs) {
+      char[] ca = str.toCharArray();
+      Arrays.sort(ca);
+      String key = String.valueOf(ca);
+
+      if (map.containsKey(key)) map.get(key).add(str);
+      else {
+        map.put(
+            key,
+            new ArrayList<String>() {
+              {
+                add(str);
+              }
+            });
+      }
+    }
+
+    result.addAll(map.values());
+    return result;
+  }
+}
