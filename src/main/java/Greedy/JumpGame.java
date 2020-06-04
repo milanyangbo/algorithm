c005d9f686dbdaa8cbd0baea7e7e989556733543
B.YANG <milanyangbo@hotmail.com>
2020-06-05 01:04:21 +0800

    11

--- src/main/java/Greedy/JumpGame.java
+++ src/main/java/Greedy/JumpGame.java
@@ -4,6 +4,13 @@ package Greedy;
 public class JumpGame {
 
   public boolean canJump(int[] nums) {
-
+    int rightmost = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (i <= rightmost) {
+        rightmost = Math.max(rightmost, i + nums[i]);
+        if (rightmost >= nums.length - 1) return true;
+      }
+    }
+    return false;
   }
 }
--- src/main/java/Greedy/JumpGameII.java
+++ src/main/java/Greedy/JumpGameII.java
@@ -8,6 +8,16 @@ public class JumpGameII {
   }
 
   public int jump(int[] nums) {
-
+    int steps = 0;
+    int maxPosition = 0;
+    int end = 0;
+    for (int i = 0; i < nums.length - 1; i++) {
+      maxPosition = Math.max(maxPosition, i + nums[i]);
+      if (i == end) {
+        end = maxPosition;
+        steps++;
+      }
+    }
+    return steps;
   }
 }
